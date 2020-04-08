1f04fe1d830bada276d045d5f140bb08c946e24b
Annabelle <ari49@uclive.ac.nz>
2020-04-08 13:37:05 +1200

    created project and some objects

---
+++ Spatial Memory in VR/Assets/CameraTracking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+
+    Camera mycam;
+    // Start is called before the first frame update
+    void Start()
+    {
+       mycam  = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        transform.LookAt(mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane)), Vector3.up);
+    }
+}
